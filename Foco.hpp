71cdea03a85fe53dff2604e19b8030e969c64fc8
diegocm19 <a23110151@live.ceti.mx>
2025-04-04 01:47:36 +0000

    práctica 2

---
+++ Foco.hpp
@@ -0,0 +1,29 @@
+
+class Foco
+{
+private:
+    bool encendido;
+
+public:
+    Foco() {
+        encendido = false;
+    }
+
+    void Encender()
+    {
+        encendido = true;
+    }
+    void Apagar()
+    {
+        encendido = false;
+    }
+    char LeerEstado()
+    {
+        if (encendido)
+        return '*';
+        else
+        return '0';
+        
+        ;
+    }
+};
---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "Foco.hpp"
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/screen.hpp>
+
+using namespace ftxui;
+
+int main (int argc, char const *argv[])
+{
+    Screen pantalla = Screen::Create(
+        Dimension::Full(),
+        Dimension::Full()
+    );
+    
+    auto document = hbox({ 
+        vbox(spinner(1,1))
+    });
+
+   ftxui::Render(pantalla,document);
+
+   
+    return EXIT_SUCCESS;
+}
