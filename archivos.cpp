9a9fed6a83464972afe1a44a365e9acf5ae1c640
diegocm19 <a23110151@live.ceti.mx>
2025-04-30 00:15:51 +0000

    Práctica 5

--- archivos.cpp
+++ archivos.cpp
@@ -3,6 +3,12 @@
 #include<string>
 using namespace std;
 
+struct Persona
+{
+    char nombre[10];
+    int edad;
+};
+
 int main(int argc, char const *argv[])
 {
 ofstream archivoSalida("archivo.txt");
@@ -15,6 +21,35 @@ return 1; // Finalizar con error
 cout << "escribiendo archivo" << endl;
 archivoSalida << "Hola mundo este es el primer archivo" << endl;
 
+archivoSalida.close();
 
+
+//-------------------------------------------------------------
+ifstream archivoEntrada("archivo.txt");
+if(!archivoEntrada){
+    cerr << "Error al leer archivo"<< endl;
+    return l; // Finalizar con error
+cout <<"Leyendo archivo" << endl;
+string linea;
+while(getline(archivoEntrada, linea)){
+    cout << linea << endl;
+}
+archivoEntrada.close();
+
+
+//-------------------------------------------------------------
+//Estructura de archivos Binarios
+ofstream archivoSalidaBinario("archivo.bin",ios::binary);
+if(archivoSalidaBinario){
+    cerr<<"Error al leer archivo"<<endl;
+    return 1;// Finalizar con error
+}
+Persona p1={"Juan",20};
+Persona p2={"Pedro",30};
+Persona p3={"Maria",40};
+
+archivoSalidaBinario.write((char*)(&p1),sizeof(Persona));
+archivoSalidaBinario.write((char*)(&p2),sizeof(Persona));
+archivoSalidaBinario.write((char*)(&p3),sizeof(Persona));
 return 0;
 }
